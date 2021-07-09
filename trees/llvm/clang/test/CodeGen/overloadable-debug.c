e441b53c838896af7477bb240c544bfdbd60d83a
Stella Laurenzo <stellaraccident@gmail.com>
2021-07-09 14:11:08 -0700

    Remove trees/llvm

--- trees/llvm/clang/test/CodeGen/overloadable-debug.c
+++
@@ -1,6 +0,0 @@
-// RUN: %clang_cc1 -triple %itanium_abi_triple -emit-llvm -debug-info-kind=limited %s -o - | FileCheck %s
-
-__attribute__((overloadable)) void f1(a) int a; {
-}
-
-// CHECK: !DISubprogram(name: "f1", linkageName: "_Z2f1i"
